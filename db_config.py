8d5af2bce2faaf758ebe4ce6b35e6bbe42760c78
f4th3r <44288832+f4th3r@users.noreply.github.com>
2019-12-22 18:39:35 +0500

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 mysqldb = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'pajke'
-app.config['MYSQL_DATABASE_PASSWORD'] = '5613Cd*aa'
-app.config['MYSQL_DATABASE_DB'] = 'project'
+app.config['MYSQL_DATABASE_USER'] = ''
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = ''
 mysqldb.init_app(app)
